7415b6e507c47f7049a0cbe9c96e73c2604c083d
EvilFlame112 <rampranav25@gmail.com>
2024-12-04 16:35:29 +0530

    add files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/nav/Navbar';
+import './App.css';
+
+function App() {
+  return(
+    <>
+      <Navbar />
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App
